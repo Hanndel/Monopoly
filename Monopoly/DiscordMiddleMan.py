aaa192374eafb6a6f3b039e6ff94a48b4018403c
Hanndel <lnote@outlook.es>
2021-09-10 22:00:52 +0200

    test

--- Monopoly/DiscordMiddleMan.py
+++ Monopoly/DiscordMiddleMan.py
@@ -19,6 +19,7 @@ async def on_message(message):
         ###Arreglar confirmar int
         ###Commits
         ###Commits
+        ###Commits
     if message.content.startswith("!Join"):
         await CG.Join(message, message.author.id)
     if message.content.startswith("!Interactuate"):
