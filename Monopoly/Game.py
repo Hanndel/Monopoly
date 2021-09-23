5fc9d55e019d2aad98f631d0cb0fe1996b161b87
Jaime <lnote@outlook.es>
2021-09-23 10:41:15 +0200

    Player and Board structure

--- Monopoly/Game.py
+++ Monopoly/Game.py
@@ -4,28 +4,70 @@ Data = {}
 
 GameBoard = {
     "0": {
-        "owner": "Bank",
+        "Name": "Something",
+        "Owner": 0,
         "Cost": 5000,
         "HouseCost": 6000,
         "Houses": 0,
         "Hotel": False,
         "Fee": 3000,
-    }
-}
-
-Recipes = {
-    "CopperWire": {
-        "Requires": {
-            "CopperBar": 1
-        }
     },
-    "Circuit": {
-        "Requires":{
-            "CopperWire": 5,
-            "IronBar": 2,
-            "SiliconBar": 2
-        }
-    }
+    "1": {
+        "Name": "Something",
+        "Owner": 0,
+        "Cost": 5000,
+        "HouseCost": 6000,
+        "Houses": 0,
+        "Hotel": False,
+        "Fee": 3000,
+    },
+    "2": {
+        "Name": "Something",
+        "Owner": 0,
+        "Cost": 5000,
+        "HouseCost": 6000,
+        "Houses": 0,
+        "Hotel": False,
+        "Fee": 3000,
+    },
+    "3": {
+        "Name": "Something",
+        "Owner": 0,
+        "Cost": 5000,
+        "HouseCost": 6000,
+        "Houses": 0,
+        "Hotel": False,
+        "Fee": 3000,
+    },
+    "4": {
+        "Name": "Something",
+        "Owner": 0,
+        "Cost": 5000,
+        "HouseCost": 6000,
+        "Houses": 0,
+        "Hotel": False,
+        "Fee": 3000,
+    },
+    "5": {
+        "Name": "Something",
+        "Owner": 0,
+        "Cost": 5000,
+        "HouseCost": 6000,
+        "Houses": 0,
+        "Hotel": False,
+        "Fee": 3000,
+    },
+    "6": {
+        "Name": "Something",
+        "Owner": 0,
+        "Cost": 5000,
+        "HouseCost": 6000,
+        "Houses": 0,
+        "Hotel": False,
+        "Fee": 3000,
+    },
+
+
 }
 
 class Game():
@@ -37,7 +79,7 @@ class Game():
 
     def AddChannel(self):
         Data[self.channelid] = {}
-        Data[self.channelid][self.channelid] = Board(self.channelid)
+        Data[self.channelid][0] = Board(self.channelid, GameBoard)
         for player in self.Players:
             Data[self.channelid][player] = Player(player, self.channelid)
 
@@ -48,40 +90,49 @@ class Game():
 
 class Player():
     def __init__(self, playerid, channelid):
-        self.playerid = playerid
-        self.channelid = channelid
-        pass
+        self.Playerid = playerid
+        self.Channelid = channelid
+        self.Money = 0
+        self.Position = 0
 
     def Roll(self):
-        print("RolledHEHE")
+        pass
+    def GiveMoney(self, player, money):
+        if (self.Money - money) > 0:
+            Data[self.Channelid][player].GetMoney(self.Playerid, money)
+            self.Money -= money
+        else:
+            self.SendMessage("You dont have enough money")
+
+    def GetMoney(self, player, money):
+        self.Money += money
+    def BuyPropertie(self, player):
+        pass
+    def SellPropertie(self, player):
+        pass
+    async def SendMessage(self,message):
+        _VarForChannel = await client.fetch_channel(self.Channelid)
+        _VarForMember = await client.fetch_user(self.Playerid)
+        _VarForChannel.send("{1.name}, {0}".format(message, _VarForMember))
+
 
 
 class Board():
-    def __init__(self, channelid):
+    def __init__(self, channelid, gameboard):
+        self.channelid = channelid
+        self.gameboard = gameboard.copy()
+        self.money = 9999999
+        self.jail  = {}
+
+    def DisplayCell(self, cell):
         pass
+    def GetMoney(self, player):
+        pass
+    def GiveMoney(self, player):
+        pass
+    async def SendMessage(self,message):
+        _VarForChannel = await client.fetch_channel(self.Channelid)
+        _VarForChannel.send("Bank, {message}".format(message))
 
 
 
-        
-
-
-
-
-
-
-
-games = 0
-
-async def Interactuate(message,idPlayer):
-    gamesList = [x for x in games.values() if idPlayer in x.playersId]
-    if(len(gamesList)>0):
-        game = gamesList[0]
-        ##comprobar accion aqui o pasar id accion y comprobar cual es en Game.py
-        game.ComprarPropiedad(idPlayer)
-    else:
-        await message.channel.send("You are not in a game currently")
-
-def ComprarPropiedad(self,idPlayer):
-    self.propertiesPlayers[idPlayer]+=1
-    print(self.propertiesPlayers)
-    return
